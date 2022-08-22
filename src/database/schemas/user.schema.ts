e17ff230624b588a64a5a60a677e934475420f16
sparsh kumar <sparshkumar14998@gmail.com>
2022-08-23 00:01:19 +0530

    added database schemas in database module.

--- src/database/schemas/user.schema.ts
+++ src/database/schemas/user.schema.ts
@@ -4,13 +4,13 @@ import { Document } from 'mongoose';
 @Schema()
 export class User {
   @Prop({ required: true })
-  username: string;
+    username: string;
 
   @Prop({ required: true })
-  email: string;
+    email: string;
 
   @Prop({ required: true })
-  password: string;
+    password: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
